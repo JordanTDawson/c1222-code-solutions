692fbdf2a874e81c672558abafda554ee26d670c
Jordan Dawson <jtdawson@icloud.com>
2022-12-12 19:02:43 +0000

    javascript-operators-and-expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,44 @@
+var width = 20;
+var height = 10;
+
+var area = height * width;
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+var payment = 250;
+var bill = 150;
+
+var change = payment - bill;
+console.log('value of change:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes = 90;
+var midterm = 85;
+var final = 95;
+
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Jordan';
+var lastName = ' Dawson';
+
+var fullName = firstName + lastName;
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var ph = 10;
+
+var isAcidic = ph < 7;
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic', typeof isAcidic);
+
+var headCount = 14;
+
+var isSparta = headCount === 300;
+console.log('value of isSparta: ', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+var motto = fullName + ' is the GOAT';
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
