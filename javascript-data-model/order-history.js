e67ecc2c27b6997aa1217120f9e4ef2b58e05c28
Jordan Dawson <jtdawson@icloud.com>
2022-12-13 18:31:34 +0000

    javascript-data-model

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,45 +1,73 @@
 var orderHistory = [
   {
-    orderPlaced: 'August 4, 2020',
-    total: '$34.00',
-    shippedTo: 'JS Masher',
-    orderNumber: '114-3941689-8772232',
-    delivered: 'August 8, 2020',
-    itemImage: 'Book',
-    itemName: 'Javascript for impatient programmers',
-    author: 'Rauschmayer, Dr. Axel',
-    finalReturnDate: 'Sep 7, 2020',
-    itemPrice: '$31.55'
+    orderDetails: [
+      {
+        orderPlaced: 'August 4, 2020',
+        total: '$34.00',
+        shippedTo: 'JS Masher',
+        orderNumber: '114-3941689-8772232',
+        delivered: 'August 8, 2020',
+        finalReturnDate: 'Sep 7, 2020'
+      }
+    ],
+    itemDetails: [
+      {
+        itemImage: 'Book',
+        itemName: 'Javascript for impatient programmers',
+        author: 'Rauschmayer, Dr. Axel',
+        itemPrice: '$31.55'
+      }
+    ]
   },
   {
-    orderPlaced: 'July 19, 2020',
-    total: 'July 19, 2020',
-    shippedTo: 'JS Masher',
-    orderNumber: '113-9984268-1280257',
-    delivered: 'Jul 20, 2020',
-    itemImage: 'Book',
-    itemName: 'The Timeless Way of Building',
-    author: 'Alexander, Christopher',
-    finalReturnDate: 'Aug 19, 2020',
-    itemPrice: '$41.33'
+    orderDetails: [
+      {
+        orderPlaced: 'July 19, 2020',
+        total: 'July 19, 2020',
+        shippedTo: 'JS Masher',
+        orderNumber: '113-9984268-1280257',
+        delivered: 'Jul 20, 2020',
+        finalReturnDate: 'Aug 19, 2020'
+      }
+    ],
+    itemDetails: [
+      {
+        itemImage: 'Book',
+        itemName: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        itemPrice: '$41.33'
+      }
+    ]
   },
   {
-    orderPlaced: 'July 4, 2020',
-    total: '$17.22',
-    shippedTo: 'JS Masher',
-    orderNumber: '114-2875557-9059409',
-    delivered: 'Jul 7, 2020',
-    itemImage: 'GameCube controller adapter',
-    itemName: 'Gamecube Controller Adapter, Super Smash Bros Switch Gamecube Adapter for Wii U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
-    finalReturnDate: 'Aug 5, 2020',
-    itemPrice: '$15.98'
+    orderDetails: [
+      {
+        orderPlaced: 'July 4, 2020',
+        total: '$17.22',
+        shippedTo: 'JS Masher',
+        orderNumber: '114-2875557-9059409',
+        delivered: 'Jul 7, 2020',
+        finalReturnDate: 'Aug 5, 2020'
+      }
+    ],
+    itemDetails: [
+      {
+        itemImage: 'GameCube controller adapter',
+        itemName: 'Gamecube Controller Adapter, Super Smash Bros Switch Gamecube Adapter for Wii U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        itemPrice: '$15.98'
+      }
+    ]
   },
   {
-    orderPlaced: 'July 3, 2020',
-    total: '$17.22',
-    shippedTo: 'JS Masher',
-    orderNumber: '113-2883177-2648248',
-    delivered: 'Jul 5, 2020',
+    orderDetails: [
+      {
+        orderPlaced: 'July 3, 2020',
+        total: '$17.22',
+        shippedTo: 'JS Masher',
+        orderNumber: '113-2883177-2648248',
+        delivered: 'Jul 5, 2020'
+      }
+    ],
     firstItem: [
       {
         itemImage: 'Gamecube Controller',
