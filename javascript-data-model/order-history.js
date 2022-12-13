94525c8a9a13aa6d11ee8e139aa64b4ce918354d
Jordan Dawson <jtdawson@icloud.com>
2022-12-13 19:45:28 +0000

    javascript-data-model

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,57 +1,54 @@
 var orderHistory = [
   {
-    orderDetails: [
-      {
-        orderPlaced: 'August 4, 2020',
-        total: '$34.00',
-        shippedTo: 'JS Masher',
-        orderNumber: '114-3941689-8772232',
-        delivered: 'August 8, 2020',
-        finalReturnDate: 'Sep 7, 2020'
-      }
-    ],
+    orderDetails: {
+      orderPlaced: 'August 4, 2020',
+      total: '$34.00',
+      shippedTo: 'JS Masher',
+      orderNumber: '114-3941689-8772232',
+      delivered: 'August 8, 2020',
+      finalReturnDate: 'Sep 7, 2020'
+    },
     itemDetails: [
       {
+        type: 'book',
         itemImage: 'Book',
-        itemName: 'Javascript for impatient programmers',
+        title: 'Javascript for impatient programmers',
         author: 'Rauschmayer, Dr. Axel',
         itemPrice: '$31.55'
       }
     ]
   },
   {
-    orderDetails: [
-      {
-        orderPlaced: 'July 19, 2020',
-        total: 'July 19, 2020',
-        shippedTo: 'JS Masher',
-        orderNumber: '113-9984268-1280257',
-        delivered: 'Jul 20, 2020',
-        finalReturnDate: 'Aug 19, 2020'
-      }
-    ],
+    orderDetails: {
+      orderPlaced: 'July 19, 2020',
+      total: 'July 19, 2020',
+      shippedTo: 'JS Masher',
+      orderNumber: '113-9984268-1280257',
+      delivered: 'Jul 20, 2020',
+      finalReturnDate: 'Aug 19, 2020'
+    },
     itemDetails: [
       {
+        type: 'book',
         itemImage: 'Book',
-        itemName: 'The Timeless Way of Building',
+        title: 'The Timeless Way of Building',
         author: 'Alexander, Christopher',
         itemPrice: '$41.33'
       }
     ]
   },
   {
-    orderDetails: [
-      {
-        orderPlaced: 'July 4, 2020',
-        total: '$17.22',
-        shippedTo: 'JS Masher',
-        orderNumber: '114-2875557-9059409',
-        delivered: 'Jul 7, 2020',
-        finalReturnDate: 'Aug 5, 2020'
-      }
-    ],
+    orderDetails: {
+      orderPlaced: 'July 4, 2020',
+      total: '$17.22',
+      shippedTo: 'JS Masher',
+      orderNumber: '114-2875557-9059409',
+      delivered: 'Jul 7, 2020',
+      finalReturnDate: 'Aug 5, 2020'
+    },
     itemDetails: [
       {
+        type: 'hardware',
         itemImage: 'GameCube controller adapter',
         itemName: 'Gamecube Controller Adapter, Super Smash Bros Switch Gamecube Adapter for Wii U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
         itemPrice: '$15.98'
@@ -59,27 +56,27 @@ var orderHistory = [
     ]
   },
   {
-    orderDetails: [
-      {
-        orderPlaced: 'July 3, 2020',
-        total: '$17.22',
-        shippedTo: 'JS Masher',
-        orderNumber: '113-2883177-2648248',
-        delivered: 'Jul 5, 2020'
-      }
-    ],
+    orderDetails: {
+      orderPlaced: 'July 3, 2020',
+      total: '$17.22',
+      shippedTo: 'JS Masher',
+      orderNumber: '113-2883177-2648248',
+      delivered: 'Jul 5, 2020'
+    },
     firstItem: [
       {
+        type: 'hardware',
         itemImage: 'Gamecube Controller',
-        itemName: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        name: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
         finalReturnDate: 'Aug 4, 2020',
         itemPrice: '$94.95'
       }
     ],
     secondItem: [
       {
+        type: 'book',
         itemImage: 'SQL Book',
-        itemName: 'The Art of SQL',
+        title: 'The Art of SQL',
         author: 'Faroult, Stephane',
         finalReturnDate: 'Aug 4, 2020',
         itemPrice: '$33.99'
