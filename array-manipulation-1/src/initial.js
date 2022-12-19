adde9e0469b78a3717697aa0fe93602cb881000c
Jordan Dawson <jtdawson@icloud.com>
2022-12-19 00:22:12 +0000

    array-manipulation-1

--- array-manipulation-1/src/initial.js
+++ array-manipulation-1/src/initial.js
@@ -1,4 +1,9 @@
 /* exported initial */
+/* create a function named initial with one parameter array
+   create a new variable with an empty array
+   create a new for loop that runs through the array's length
+   inside of the code block for the for loop push the array values to the empty array
+   return the array to the function so the code block ends */
 function initial(array) {
   var initial = [];
   for (var i = 0; i < array.length - 1; i++) {
