e8d1b3d7d02a19e52d986df466d4db01b6dadf20
Jordan Dawson <jtdawson@icloud.com>
2022-12-21 19:32:38 +0000

    javascript-light-bulb

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,11 +1,19 @@
-/* var buttonOff = document.querySelector('.button-off');
-var bodyOff = document.querySelector('body-off');
+var button = document.querySelector('button');
+var body = document.querySelector('body');
+var buttonOff = document.querySelector('.button-off');
+var buttonOn = document.querySelector('.button-on');
+var bodyOff = document.querySelector('.body-off');
+var bodyOn = document.querySelector('.body-on');
 var isOn = false;
 function handleClick(event) {
-  buttonOff.className =
-  bodyOff.className =
-  console.log(button);
-  console.log(isOn);
+  if (isOn === false) {
+    body.className(bodyOff);
+    button.className(buttonOff);
+  } else {
+    body.className(bodyOn);
+    button.className(buttonOn);
+  }
+  isOn = !isOn;
 }
 
-button.addEventListener('click', handleClick); */
+button.addEventListener('click', handleClick);
