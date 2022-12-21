10b6b162be2f0ac223fbb1f52d53396422a72e7f
Jordan Dawson <jtdawson@icloud.com>
2022-12-21 17:33:28 +0000

    javascript-light-bulb

---
+++ javascript-light-bulb/main.js
@@ -0,0 +1,11 @@
+/* var buttonOff = document.querySelector('.button-off');
+var bodyOff = document.querySelector('body-off');
+var isOn = false;
+function handleClick(event) {
+  buttonOff.className =
+  bodyOff.className =
+  console.log(button);
+  console.log(isOn);
+}
+
+button.addEventListener('click', handleClick); */
