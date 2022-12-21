498ec81a6075c70f4368c2d61c4745151fbd0d98
Jordan Dawson <jtdawson@icloud.com>
2022-12-21 19:49:50 +0000

    dom-delegation w guard clause example

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -9,3 +9,18 @@ function handleClick(event) {
   }
 }
 taskList.addEventListener('click', handleClick);
+
+/*
+.addEventListener('click', function (event) { //this function is an anonymous function.
+  console.log('event.target: ', event.target);
+  console.log('event.target.tagetName: ', event.target.tagName);
+
+  if (event.target.tagName !== 'BUTTON') { // guard clause a.k.a a good habit to start.
+    return;
+  }
+
+  var taskListItem = event.target.closest('.task-list-item');
+  console.log('closest .task-list-item', taskListItem);
+  taskListItem.remove();
+});
+*/
