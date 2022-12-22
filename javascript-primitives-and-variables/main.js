d19d1d5594d33e63143839c4f38cf07cb94126c7
Jordan Dawson <jtdawson@icloud.com>
2022-12-22 19:33:07 +0000

    javascript-redo

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,15 @@
+var fullName = 'Jordan Dawson';
+var isCool = true;
+var totalPets = 2;
+
+console.log('Full name: ', fullName);
+console.log('Am I cool?: ', isCool);
+console.log('Total pets: ', totalPets);
+
+console.log('Full name typeof: ', typeof fullName);
+console.log('Am I cool typeof: ', typeof isCool);
+console.log('Total pets typeof: ', typeof totalPets);
+
+var unknown = null;
+console.log('Unknown: ', unknown);
+console.log('Unknown typeof: ', unknown);
