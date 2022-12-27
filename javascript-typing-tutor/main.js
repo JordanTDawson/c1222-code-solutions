3578721564d1bccae25825e847330eaa673727a1
Jordan Dawson <jtdawson@icloud.com>
2022-12-27 08:39:47 +0000

    javascript-typing-tutor

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,19 @@
+/*
+var span = document.querySelectorAll('span');
+var right = document.querySelector('.right');
+var wrong = document.querySelector('.wrong');
+
+var strArray = [];
+strArray.push(span);
+
+document.addEventListener('keydown', () => {
+  var val = span.value;
+  val.split('').map((letter, i) => {
+    if (letter === str[i]){
+      strArray[i].classList.add("correct")
+    } else {
+
+    }
+  })
+});
+*/
