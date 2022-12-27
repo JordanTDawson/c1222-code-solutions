01c1f613848d69abeb61dfd8be36808bba25256d
Jordan Dawson <jtdawson@icloud.com>
2022-12-27 17:50:34 +0000

    javascript-typing-tutor

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,19 +1,9 @@
-/*
+/* we want to select all span elements in html.
+   create a new array for all the span elements to be pushed into.
+   push all the items from span into the new array.
+   create a document event listener that listens for keydown
+   create an if statement  */
 var span = document.querySelectorAll('span');
-var right = document.querySelector('.right');
-var wrong = document.querySelector('.wrong');
 
 var strArray = [];
 strArray.push(span);
-
-document.addEventListener('keydown', () => {
-  var val = span.value;
-  val.split('').map((letter, i) => {
-    if (letter === str[i]){
-      strArray[i].classList.add("correct")
-    } else {
-
-    }
-  })
-});
-*/
