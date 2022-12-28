d788a4d0d5803ea778a770e2fd4989f3d4abf898
Jordan Dawson <jtdawson@icloud.com>
2022-12-28 18:15:33 +0000

    sorry I actually did the excercise but put the before unload in the main.js file haha

--- javascript-local-storage/data.js
+++ javascript-local-storage/data.js
@@ -1,5 +1,11 @@
 /* exported todos */
 
+window.addEventListener('beforeunload', function (event) {
+  var todosJSON = JSON.stringify(todos);
+  localStorage.setItem('javascript-local-storage', todosJSON);
+
+});
+
 var todos = [];
 var previousTodosJSON = localStorage.getItem('javascript-local-storage');
 if (previousTodosJSON !== null) {
