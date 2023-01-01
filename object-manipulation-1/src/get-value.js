433a1cc679faa603dac7d22b3f3325f6c3e14773
JordanTDawson <jtdawson@icloud.com>
2023-01-01 19:35:54 +0000

    changes made to object manipulation

--- object-manipulation-1/src/get-value.js
+++ object-manipulation-1/src/get-value.js
@@ -4,8 +4,5 @@
    in the for in loops code block value equals object at key
    close the cdoe block and return the value. */
 function getValue(object, key) {
-  for (var value in object) {
-    value = object[key];
-  }
-  return value;
+  return object[key];
 }
