e1810d3731297424c8554d26f697fd97360ccda5
JordanTDawson <jtdawson@icloud.com>
2023-01-02 05:29:15 +0000

    started

--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,1 +1,12 @@
 /* exported ransomCase */
+function ransomCase(string) {
+  var ransomCaseString = '';
+  for (var i = 0; i < string.length; i++) {
+    if (i % 2 !== 0) {
+      ransomCaseString += string[i].toUpperCase();
+    } else {
+      ransomCaseString += string[i].toLowerCase();
+    }
+  }
+  return ransomCaseString;
+}
--- string-manipulation-2/src/truncate.js
+++ string-manipulation-2/src/truncate.js
@@ -1,1 +1,7 @@
 /* exported truncate */
+/* create a new function named truncate with two parameters length and string
+   in the functions code block we want to split the string at a specific length
+   then we would concantenate '...' to the split string and return the new string */
+function truncate(length, string) {
+  return string.substring(0, length) + '...';
+}
