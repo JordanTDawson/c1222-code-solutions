740ac706a597d11393495e0642d40e48d1a58b5c
Jordan Dawson <jtdawson@icloud.com>
2023-01-03 18:25:50 +0000

    started object-manipulation-2

---
+++ object-manipulation-2/src/pick.js
@@ -0,0 +1,10 @@
+/* exported pick */
+/* create a function named pick with two arguements source and keys
+   create a variable that stores the pulled objects in a new object
+   create an if statement with a condition that checks that key values from source
+   if the condition is true send the key to new object
+   close function codeblock and return the new Object. */
+// function pick(source, keys) {
+//   var pickedObjects = {
+//   };
+// }
