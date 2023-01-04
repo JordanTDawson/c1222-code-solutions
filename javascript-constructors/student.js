3fe3356fc445be85a719e4b109e28853f1741a55
JordanTDawson <jtdawson@icloud.com>
2023-01-04 07:06:38 +0000

    need to finish student.js

--- javascript-constructors/student.js
+++ javascript-constructors/student.js
@@ -1,1 +1,17 @@
 /* exported Student */
+function Student(firstName, lastName, subject) {
+  var student = {
+    firstName: 'Jordan',
+    lastName: 'Dawson',
+    subject: 'JavaScript',
+    getFullName: function () {
+      return this.firstName + ' ' + this.lastName;
+    },
+    getIntroduction: function () {
+      var introduction = 'Hello, my name is ' + this.getFullName() + ' and I am studying ' +
+      this.subject + '.';
+      return introduction;
+    }
+  };
+  return student;
+}
