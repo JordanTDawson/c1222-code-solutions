05bc125f2860e588d5d7721f285b9a4d1227f468
Jordan Dawson <jtdawson@icloud.com>
2023-01-05 17:55:18 +0000

    restarted javascript carousel

---
+++ javascript-carousel/main.js
@@ -0,0 +1,13 @@
+// var chevronRight = document.querySelector('#chevron-right');
+// var chevronleft = document.querySelector('#chevron-left');
+// var slideIndex = 1;
+// function handleChevronLeftClick(event) {
+
+// }
+
+// function handleChevronRightClick(event) {
+
+// }
+
+// chevronleft.addEventListener('click', handleChevronLeftClick);
+// chevronRight.addEventListener('click', handleChevronRightClick);
